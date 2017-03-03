af1208aaf98463bb7e9cfa387692f6f41505ec54
andyafter <andyafter@gmail.com>
2017-03-03 11:15:24 +0800

    Website seed and testcsv

--- testcsv.py
+++ testcsv.py
@@ -1,6 +1,18 @@
 import csv
 
+csvreader = {}
+text = []
 with open('sample.csv', 'rb') as csvfile:
-    spamreader = csv.reader(csvfile)
-    for row in spamreader:
-        print ', '.join(row)
+    csvreader = csv.reader(csvfile)
+    for row in csvreader:
+        info = ','.join(row[3:]).replace('\\', '').replace('\"', '').replace('{', '').replace('}', '')
+        info_obj = {}
+        last_obj = ''
+        for obj in info.split(','):
+            if len(obj.split(':')) == 1:
+                info_obj[last_obj] = info_obj[last_obj]+','+obj
+                continue
+            key, item = obj.split(':')
+            info_obj[key] = item
+            last_obj = key
+        print info_obj
